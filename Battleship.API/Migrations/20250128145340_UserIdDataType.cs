cc64ecd64db33e4f35d526fcc27c26fecb387b48
ajpiccolo <ajpiccolo@gmail.com>
2025-01-28 10:05:31 -0500

    update FK data types referencing User

---
+++ Battleship.API/Migrations/20250128145340_UserIdDataType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Battleship.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserIdDataType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Boards",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Boards",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- Battleship.API/src/1_Model/Board.cs
+++ Battleship.API/src/1_Model/Board.cs
@@ -4,6 +4,6 @@ public class Board
 {
     public int Id { get; set; }
     public int GameId { get; set; } // Foreign Key to Game
-    public int UserId { get; set; } // Foreign Key to User
+    public string UserId { get; set; } // Foreign Key to User
     public bool IsComputerBoard { get; set; }
 }
